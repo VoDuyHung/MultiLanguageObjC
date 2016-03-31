c118a5a08366976da22c905560413b42a3e9dc0f
VoDuyHung <voduyhung58@gmail.com>
2016-03-31 13:50:19 +0700

    Initial Commit

---
+++ LocalizationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalizationApp
+//
+//  Created by DUY TAN on 31/3/16.
+//  Copyright © 2016 DUY TAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalizationApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalizationApp
+//
+//  Created by DUY TAN on 31/3/16.
+//  Copyright © 2016 DUY TAN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
